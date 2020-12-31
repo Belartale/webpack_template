80cfa7059b266d14a549f5b0cd11342b2766b476
belartale <belartale@gmail.com>
2020-12-31 22:46:18 +0200

    save

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+	context: path.resolve(__dirname) // папка
+	entry: "./src/js/index.js", // труба .pipe
+	output: {
+		filename: "",
+		path: "",
+	},
+};
