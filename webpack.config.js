cc33d5d84a8a782681c3c144816550dfae67b4bd
belartale <belartale@gmail.com>
2021-01-01 02:31:28 +0200

    save

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require("path");
 
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}`
+
 module.exports = {
-	context: path.resolve(__dirname) // папка
+	context: path.resolve(__dirname, "src"), // папка
+	mode: "development",
 	entry: "./src/js/index.js", // труба .pipe
 	output: {
-		filename: "",
-		path: "",
+		filename: `./js/${filename("js")}`,
+		path: path.resolve(__dirname, "app"),
 	},
 };
