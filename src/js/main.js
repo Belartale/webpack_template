ca6f5c24f41864d528c77c604e3911e1d5369dc3
belartale <belartale@gmail.com>
2021-01-02 18:10:18 +0200

    add styleline

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 // import "../main.css";
 // import $ from "jquery";
 
+import "../../node_modules/focus-visible/dist/focus-visible";
+
 import "../scss/main.scss";
 import "../index.html";
 
 const mytest = "dfsdf";
+if (condition) {
+  const a = 1;
+}
